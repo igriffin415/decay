447fc0a7d6e54413159240a0a56bb255a4b50daf
igriffin415 <fireice16@yahoo.com>
2017-12-07 18:09:50 -0500

    nn

--- src/DecayApp.java
+++ src/DecayApp.java
@@ -1,101 +1,87 @@
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
+import processing.core.*;
+ 
+public class DecayApp extends PApplet {
+        // define a buffer to draw the rectangles on
+		public static float PROJECTOR_RATIO = 1080f/1920.0f;
+		public float top, bottom;
+        PGraphics buffer;
+        
+        public void settings() {
+    		createWindow(true, false, .5f);
+    	}
+        
+        public void setup() {
+             // create the buffer to be the same size as the applet
+                buffer = createGraphics(width, height, P2D); 
+        }
+ 
+        
+        public void draw() {  
+        		
+                // Fade everything on the applet
+                noStroke();
+                fill( 255, 255, 255, 100);
+                rect(0, 0, width, height);
 
-import processing.core.PApplet;
-import processing.core.PImage;
-import processing.core.PVector;
+                // change color of circle paint depending on mouse button
+                if (mousePressed)  {
+                        stroke( 255, 255, 255 );
+                        fill( 0,0,0 );
+                }
+                else { 
+                        stroke( 0, 0, 0 );
+                        fill( 255, 255, 255 );
+                        
+                        
+                }
+                
 
-public class DecayApp extends PApplet{
-	public static float PROJECTOR_RATIO = 1080f/1920.0f;
-	
-	String recordingFile = "test.kinect";
-	KinectBodyDataProvider kinectReader;
-	PersonTracker tracker = new PersonTracker();
-	
-	LinkedHashMap<Long, Person> people;
-	
-	PImage fadeImage;
-	
-	
-	public void settings() {
-		createWindow(true, false, .5f);
-	}
+                // draw a rectangle on the buffer, offset by 100 pixels right and down
+                // from the mouse cursor.               
+                buffer.beginDraw();
+                buffer.rectMode( CENTER );
+                buffer.fill(255, 255, 255, 100);
+                buffer.rect(0, 0, width, height);
+                buffer.fill(100);
+                buffer.rect( mouseX + 100 , mouseY + 100, 50, 50 );
+                buffer.endDraw();
+                // overlay the buffer on the applet
+                image(buffer, 0, 0);
+                
+             // draw a circle where the mouse is located
+                ellipse( mouseX, mouseY, 50, 50 );
+                
+            
+        }
+        
+        public static void main(String[] args) {
 
-	public void setup() {	
-		try {
-			kinectReader = new KinectBodyDataProvider("test.kinect", 10);
-		} catch (IOException e) {
-			System.out.println("Unable to create kinect producer");
-		}
-				 
-		//kinectReader = new KinectBodyDataProvider(8008);
-		
-		people = new LinkedHashMap<Long, Person>();
-		
-		kinectReader.start();
-	}
+    		PApplet.main(DecayApp.class.getName());
+    	}
+        
+        public void createWindow(boolean useP2D, boolean isFullscreen, float windowsScale) {
+    		if (useP2D) {
+    			if(isFullscreen) {
+    				fullScreen(P2D);  			
+    			} else {
+    				size((int)(1920 * windowsScale), (int)(1080 * windowsScale), P2D);
+    			}
+    		} else {
+    			if(isFullscreen) {
+    				fullScreen();  			
+    			} else {
+    				size((int)(1920 * windowsScale), (int)(1080 * windowsScale));
+    			}
+    		}		
+    	}
 
-	public void draw() {
-		fadeImage = loadImage("assets/featuredBGFade.png");
-		fadeImage.resize(this.displayWidth, this.displayHeight);
-		setScale(0.5f);
-		imageMode(CENTER);
-		background(0,0,0);
-		
-		KinectBodyData bodyData = kinectReader.getData();
-		
-		tracker.update(bodyData);
-		
-		 Iterator<Body> i = tracker.getPeople().values().iterator();
-		 if(i.hasNext()) {
-			Body b = i.next();
-			Person p;
-			p = people.get(b.getId());
-			if(p== null) {
-				 p = new Person(this);
-				 people.put(b.getId(), p);
-			}
-			for(Long id : people.keySet()) {
-				if(! tracker.getPeople().keySet().contains(id)) {
-					people.remove(id);
-				}
-			}
-
-			p.update(b);
-			p.drawRibs();
-			p.drawHead();
-		 }
-	}
-	
-	
-	
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	public void createWindow(boolean useP2D, boolean isFullscreen, float windowsScale) {
-		if (useP2D) {
-			if(isFullscreen) {
-				fullScreen(P2D);  			
-			} else {
-				size((int)(1920 * windowsScale), (int)(1080 * windowsScale), P2D);
-			}
-		} else {
-			if(isFullscreen) {
-				fullScreen();  			
-			} else {
-				size((int)(1920 * windowsScale), (int)(1080 * windowsScale));
-			}
-		}		
-	}
-	
-	// use lower numbers to zoom out (show more of the world)
-	// zoom of 1 means that the window is 2 meters wide and appox 1 meter tall.
-	public void setScale(float zoom) {
-		scale(zoom* width/2.0f, zoom * -width/2.0f);
-		translate(1f/zoom , -PROJECTOR_RATIO/zoom );		
-	}
-	
-	public static void main(String[] args) {
-		PApplet.main(DecayApp.class.getName());
-	}
+    	// use lower numbers to zoom out (show more of the world)
+    	// zoom of 1 means that the window is 2 meters wide and appox 1 meter tall.
+    	public void setScale(float zoom) {
+    		scale(zoom* width/2.0f, zoom * -width/2.0f);
+    		translate(1f/zoom , -PROJECTOR_RATIO/zoom );
+    		top = 1f/zoom;
+    		bottom = -PROJECTOR_RATIO/zoom;
+    		
+    	}
 }
